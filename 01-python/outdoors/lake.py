a30f06de56862d057ba63035ccea6ade354008a3
playerInEminor <984336573@qq.com>
2020-01-27 17:47:27 -0500

    lake

--- 01-python/outdoors/lake.py
+++ 01-python/outdoors/lake.py
@@ -1,3 +1,15 @@
-def draw_lake():
-    print("lake not found")
+import math
+
+def draw_lake(area=1, shape="square"):
+    if(shape=="square"):
+        for i in range(math.sqrt(area)):
+            print("-")
+        for i in range(math.sqrt(area)):
+            print("|" + math.sqrt(area)*" " + "|")
+        for i in range(math.sqrt(area)):
+            print("-")
+    # n =math.sqrt(area) - 1
+    # if(shape=="triangle"):
+    #     for i in range(1, area + 1):
+    #         print(math.sqrt(area) - 1）i*"*")
     return
