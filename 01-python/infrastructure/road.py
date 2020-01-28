f7193b2b057ecbd840a07d23194b1b35a90e4cdb
playerInEminor <984336573@qq.com>
2020-01-28 14:18:37 -0500

    fix bugs and delete comments

--- 01-python/infrastructure/road.py
+++ 01-python/infrastructure/road.py
@@ -1,3 +1,8 @@
-def draw_road():
-    print("road not found")
+def draw_road(length=10, axis="horizontal"):
+    if axis == "horizontal":
+        for i in range(length):
+            print("=")
+    else:
+        for i in range(length):
+            print("||")
     return
--- 01-python/outdoors/lake.py
+++ 01-python/outdoors/lake.py
@@ -1,15 +1,12 @@
 import math
 
-def draw_lake(area=1, shape="square"):
+def draw_lake(area=9, shape="square"):
     if(shape=="square"):
-        for i in range(math.sqrt(area)):
-            print("-")
-        for i in range(math.sqrt(area)):
-            print("|" + math.sqrt(area)*" " + "|")
-        for i in range(math.sqrt(area)):
-            print("-")
-    # n =math.sqrt(area) - 1
-    # if(shape=="triangle"):
-    #     for i in range(1, area + 1):
-    #         print(math.sqrt(area) - 1）i*"*")
+        n = int(math.sqrt(area))
+        print(" " + n*"-")
+        for i in range(n):
+            print("|" + n*" " + "|")
+        print(" " + n * "-")
+    else:
+        print("Sorry, now the shape should be square.")
     return
